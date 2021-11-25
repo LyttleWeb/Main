d8466ef4cd6094f40a1b1667afcf9504f39bd4fa
LyttleWeb <95014694+LyttleWeb@users.noreply.github.com>
2021-11-25 03:14:14 -0400

    Add files via upload

---
+++ Assignment/piechart.js
@@ -0,0 +1,42 @@
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Plastic Waste'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.2f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Plastic Uses',
+        colorByPoint: true,
+        data: [{
+            name: 'Unrecycled',
+            y: 91.00,
+            sliced: true,
+            selected: true
+        },  {
+            name: 'Recycled',
+            y: 9.00
+        }]
+    }]
+});
